d82b17a3a6627bd41b2f20c05e3ae988abeb8018
Estevao Borges De Lara <estevao.lara@metainf.com.br>
2017-10-25 10:40:32 -0200

    Site atualizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,115 +1,101 @@
-var notes = window.localStorage.getItem('notes') || '{"data":[]}';
-   notes = JSON.parse(notes);
+let notes = window.localStorage.getItem('notes') || '{"data": []}';
+notes = JSON.parse(notes);
 
+let updateList = function () {
+    console.log('[Application] start watch');
 
-var updateList = function () {
-    console.log('[Application] started watch');
     Array.observe(notes.data, function (changes) {
-        var index = null;
-        var value = '';
-        var status = null;
+        let index = null;
+        let value = '';
+        let status = null;
 
-        if(changes[0].type === 'splice'){
+        if (changes[0].type === 'splice') {
             index = changes[0].index;
             value = changes[0].object[index];
-            status = (changes[0].addedCount > 0 ) ? 'created' : 'removed';
-
+            status = (changes[0].addedCount > 0) ? 'created' : 'removed';
         }
 
-        if(changes[0].type === 'update'){
+        if (changes[0].type === 'update') {
             index = changes[0].name;
             value = changes[0].object[index];
             status = 'updated';
         }
 
-        if(!value && status === 'created' && status === 'updated'){
+        if (!value && status === 'created' && status === 'updated') {
             return;
         }
 
-        var notesTag = document.getElementById('notas');
+        let notesTag = document.getElementById('notes');
 
-        if(status === 'updated'){
-            console.log('Implementar');
+        if (status === 'updated') {
+            console.log('implementar');
         }
 
-        if(status === 'removed'){
-            var listOfNotes = document.querySelectorAll("#notas li");
-
-            notesTag.removeChild(listOfNotes[index])
+        if (status === 'removed') {
+            let listOfNotes = document.querySelectorAll('#notes li');
+            notesTag.removeChild(listOfNotes[index]);
         }
 
-        if(status === 'created'){
-
-            var newLi = document.createElement('li');
-
+        if (status === 'created') {
+            let newLi = document.createElement('li');
             newLi.innerHTML = value;
-
             notesTag.appendChild(newLi);
         }
 
         window.localStorage.setItem('notes', JSON.stringify(notes));
     });
-};
+}
 
-var createNote = function () {
-    var input = document.querySelector('#form-add-note input[type="text"]');
-    var value = input.value;
+let createNote = function () {
+    let input = document.querySelector('#form-add-note input[type="text"]');
+    let value = input.value;
 
     notes.data.push(value);
 
     input.value = "";
-
-
-};
+}
 
 updateList();
 
 document.addEventListener('DOMContentLoaded', function (event) {
+    let listOfNotes = document.getElementById('notes');
+    let listHtml = '';
 
-    var listOfNotes = document.getElementById('notas');
-    var listHtml = '';
-
-    for(var i = 0; i < notes.data.length; i++){
-        listHtml += '<li>'+ notes.data[i] +'</li>';
+    for(let i=0; i< notes.data.length; i++) {
+        listHtml += '<li>' + notes.data[i] +  '</li>';
     }
 
     listOfNotes.innerHTML = listHtml;
 
-    var formAddNotes = document.getElementById('form-add-note');
-
+    let formAddNotes = document.getElementById('form-add-note')
     formAddNotes.addEventListener('submit', function (e) {
         e.preventDefault();
-
         createNote();
     });
-
 });
 
 document.addEventListener('click', function (e) {
+    let notesTag = document.getElementById('notes');
 
-   var notesTag = document.getElementById('notas');
-
-   if(e.target.parentElement === notesTag){
-
-        if(confirm('Remover esta nota?')){
-            var listOfNotes = document.querySelectorAll('#notas li');
-            listOfNotes.forEach( function (item, index ) {
-                if(e.target === item ){
+    if (e.target.parentElement === notesTag) {
+        if (confirm('remover esta nota?')) {
+            let listOfNotes = document.querySelectorAll('#notes li');
+            listOfNotes.forEach(function (item, index) {
+                if (e.target === item) {
                     notes.data.splice(index, 1);
                 }
             });
         }
-   }
-
+    }
 });
 
-if('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('./service-worker.js')
-        .then(function (reg) {
-           console.log('Service worker Registered');
-        })
-        .catch(function (err) {
-            console.log('Erro', err);
-        });
+    .register('./service-worker.js')
+    .then(function(reg) {
+        console.log('Service worker Registered');
+    })
+    .catch(function (err) {
+        console.log('erro', err);
+    });
 }
--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,31 @@
-var cacheName = 'notes-son-v.1.0.0.0.0.0';
-
-var filesToCache = [
+let cacheName = 'notes-son-v.1.0.0';
+let filesToCache = [
     './',
     'index.html',
-    'css/bootstrap.css',
-    'css/bootstrap.min.css',
+    'css/colors.css',
     'css/styles.css',
-    'js/array.observe.pollyfill.js',
-    'js/bootstrap.js',
-    'js/bootstrap.min.js',
-    'js/jquery-3.2.1.min.js',
-    'js/npm.js',
-    'js/object.observe.pollyfill.js',
+    'js/array.observe.polyfill.js',
+    'js/object.observe.polyfill.js',
     'js/scripts.js'
 ];
 
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Instaler');
-
+    console.log('[ServiceWorker] Installer');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching App shell');
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
-    )
+    );
 });
 
 self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
         caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if(key !== cacheName){
-                    console.log('[ServiceWorker] Removing old cache');
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -42,10 +35,9 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
-
     e.respondWith(
-        caches.match(e.request).then(function (response) {
-          return response || fetch(e.request);
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
-    )
+    );
 });
